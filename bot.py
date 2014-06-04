2cf6a458915a9ed5997018c4dec247795c51f5c2
Adjective-Object <mhuan13@gmail.com>
2014-06-04 15:25:13 -0400

    -as

--- bot.py
+++ bot.py
@@ -8,9 +8,25 @@ mongoconn = None
 
 hostport = 65152
 
+def signal_handler(signal, frame):
+    #cleanup mongo connection n interrupt
+    mongoconn.disconnect();
+    sys.exit()
+
 app = flask.Flask(__name__)
 apitokens = ["token", "team_id", "channel_id", "channel_name", "timestamp",
         "user_id", "user_name", "text"]
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        hostport = int(sys.argv[1])
+    mongoconn = pymongo.MongoClient(mongoaddr, mongoport)
+    signal.signal(signal.SIGINT, signal_handler)
+
+
+
+app.config["SERVER_NAME"] = "127.0.0.1:"+str(hostport)
+
 
 @app.route('/bot', methods=['POST'])
 def onCall():
@@ -22,21 +38,5 @@ def onCall():
         return "{text:\"logging confirmed\"}\n"
     return "i dun get my props..\n"
 
-def signal_handler(signal, frame):
-    #cleanup mongo connection n interrupt
-    mongoconn.disconnect();
-    sys.exit()
-
 if __name__ == "__main__":
-    print "bootup"
-
-    if(len(sys.argv) > 1):
-        hostport = int(sys.argv[1])
-
-    app.config["SERVER_NAME"] = "127.0.0.1:"+str(hostport)
-
-
-    mongoconn = pymongo.MongoClient(mongoaddr, mongoport)
-    signal.signal(signal.SIGINT, signal_handler)
-
     app.run(host='0.0.0.0')
